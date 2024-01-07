73089a9f6b0c81672d878c29e01a665b2e61bbd8
Toook58 <132582463+Toook58@users.noreply.github.com>
2024-01-07 19:51:14 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/bash
-export NEZHA_SERVER="******:5555"
-export NEZHA_KEY="******"
+export NEZHA_SERVER="nz.tomes.eu.org:443"
+export NEZHA_KEY="9UD7TmTSCptqNWyXW9"
 
 chmod +x swith start.sh
-nohup ./swith -s ${NEZHA_SERVER} -p ${NEZHA_KEY} > /dev/null 2>&1 &   #需要tls在 > 前面加上 --tls即可
+nohup ./swith -s ${NEZHA_SERVER} -p ${NEZHA_KEY} --tls > /dev/null 2>&1 &   #需要tls在 > 前面加上 --tls即可
 
 tail -f /dev/null
